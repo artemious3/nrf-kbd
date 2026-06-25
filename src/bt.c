ee6ac8072b0443eaf41e3f46ef30a4854c0e7e8b
Artsiom Padhaiski <artempodgaisky@gmail.com>
2026-06-25 12:04:04 +0300

    move some defines to header and change KEY_PRESS_MAX to 8

--- src/bt.c
+++ src/bt.c
@@ -50,33 +50,6 @@
 /* HIDs queue elements. */
 #define HIDS_QUEUE_SIZE 10
 
-/* ********************* */
-/* Buttons configuration */
-
-/* Note: The configuration below is the same as BOOT mode configuration
- * This simplifies the code as the BOOT mode is the same as REPORT mode.
- * Changing this configuration would require separate implementation of
- * BOOT mode report generation.
- */
-#define KEY_CTRL_CODE_MIN 224 /* Control key codes - required 8 of them */
-#define KEY_CTRL_CODE_MAX 231 /* Control key codes - required 8 of them */
-#define KEY_CODE_MIN      0   /* Normal key codes */
-#define KEY_CODE_MAX      101 /* Normal key codes */
-#define KEY_PRESS_MAX \
-    6 /* Maximum number of non-control keys \
-       * pressed simultaneously \
-       */
-
-/* Number of bytes in key report
- *
- * 1B - control keys
- * 1B - reserved
- * rest - non-control keys
- */
-#define INPUT_REPORT_KEYS_MAX_LEN (1 + 1 + KEY_PRESS_MAX)
-
-/* Current report map construction requires exactly 8 buttons */
-BUILD_ASSERT((KEY_CTRL_CODE_MAX - KEY_CTRL_CODE_MIN) + 1 == 8);
 
 /* OUT report internal indexes.
  *
--- src/bt.h
+++ src/bt.h
@@ -2,6 +2,35 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+
+/* ********************* */
+/* Buttons configuration */
+
+/* Note: The configuration below is the same as BOOT mode configuration
+ * This simplifies the code as the BOOT mode is the same as REPORT mode.
+ * Changing this configuration would require separate implementation of
+ * BOOT mode report generation.
+ */
+#define KEY_CTRL_CODE_MIN 224 /* Control key codes - required 8 of them */
+#define KEY_CTRL_CODE_MAX 231 /* Control key codes - required 8 of them */
+#define KEY_CODE_MIN      0   /* Normal key codes */
+#define KEY_CODE_MAX      101 /* Normal key codes */
+#define KEY_PRESS_MAX \
+    8 /* Maximum number of non-control keys \
+       * pressed simultaneously \
+       */
+
+/* Number of bytes in key report
+ *
+ * 1B - control keys
+ * 1B - reserved
+ * rest - non-control keys
+ */
+#define INPUT_REPORT_KEYS_MAX_LEN (1 + 1 + KEY_PRESS_MAX)
+
+/* Current report map construction requires exactly 8 buttons */
+BUILD_ASSERT((KEY_CTRL_CODE_MAX - KEY_CTRL_CODE_MIN) + 1 == 8);
+
 /** @brief Release the button and send report
  *
  *  @note Functions to manipulate hid state are not reentrant
